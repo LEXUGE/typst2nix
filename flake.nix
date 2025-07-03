1d9339e9fe047e0b7a7cfc0488e58882a57d8304
Harry Ying <lexugeyky@outlook.com>
2025-07-03 02:07:46 +0100

    refactor: use nixpkgs infrastructure
    typst2nix now uses standardized `nixpkgs.typstPackages` and the
    dependency information in `passthru.typstDeps` of each package

--- flake.nix
+++ flake.nix
@@ -8,259 +8,195 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    official-packages = {
-      url = "github:typst/packages";
-      flake = false;
-    };
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, utils, pre-commit-hooks, rust-overlay, official-packages, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      pre-commit-hooks,
+      ...
+    }:
     with utils.lib;
     with nixpkgs.lib;
     with builtins;
-    rec {
-      overlays = rec {
-        default = (final: prev: {
-          # Merging with prev.typst2nix.registery makes sure that order of overlay application doesn't matter
-          typst2nix = {
-            registery =
-              # Get prev if exists
-              (prev.typst2nix.registery or { }) // (mapAttrsRecursive
-                (n: v: (helpers.bundleTypstPkg
-                  {
-                    pkgs = final;
-                    path = v;
-                    namespace = head n;
-                  }))
-                (helpers.listPackages "${official-packages}/packages"));
-          };
-          typst-package-bunlder = self.packages."${prev.pkgs.system}".typst-package-bunlder;
-        });
-      };
-
+    {
       helpers = rec {
-        mkTypstEnv = pkgs: src:
+        # Control root with `src`, control the entry point with `main`
+        buildTypstDoc =
+          {
+            pkgs,
+            pname,
+            version,
+            src,
+            main,
+            ext ? "pdf",
+            inputs ? { },
+          }:
           let
-            registery = pkgs.typst2nix.registery;
-            dependencies = getDependencies [ ] registery src;
-          in
-          pkgs.symlinkJoin {
-            name = "typstEnv";
-            paths = (map (p: attrByPath p.path null registery) dependencies);
-          };
-
-        mkWrappedTypst = pkgs: src: with pkgs; (runCommand "typst-wrapped"
-          { nativeBuildInputs = [ makeWrapper ]; }
-          ''
-            mkdir -p $out/bin
-            makeWrapper ${typst}/bin/typst $out/bin/typst --set XDG_DATA_HOME ${mkTypstEnv pkgs src}
-          '');
-
-
-        # Control root with `src`, control the entry point with `path`
-        buildTypstDoc = { pkgs, pname, version, src, path, ext ? "pdf", inputs ? { } }:
-          let
-            convertedInputs = foldr (a: b: a + " " + b) "" (mapAttrsToList (n: v: "--input " + n + "=" + v) inputs);
+            convertedInputs = foldr (a: b: a + " " + b) "" (
+              mapAttrsToList (n: v: "--input " + n + "=" + v) inputs
+            );
           in
           (pkgs.stdenv.mkDerivation rec {
             inherit pname version src;
 
-            buildInputs = [ (mkWrappedTypst pkgs src) ];
-            # Alternatively, use
-            # env = {
-            #   XDG_DATA_HOME = (mkTypstEnv pkgs src);
-            # };
+            buildInputs = [
+              (pkgs.typst.withPackages (extractDependencies {
+                path = src;
+              }))
+            ];
 
             buildPhase = ''
               mkdir $out
-              typst compile ${path} $out/${pname}.${ext} --root . ${convertedInputs}
+              typst compile ${main} $out/${pname}.${ext} --root . ${convertedInputs}
             '';
           });
 
-        # TODO: implement `exclude` keyword.
-        bundleTypstPkg = { pkgs, path, namespace, }:
-          let
-            typstManifest = (importTOML (path + "/typst.toml"));
-            registery = pkgs.typst2nix.registery;
-          in
-          (pkgs.stdenv.mkDerivation rec {
-            pname = typstManifest.package.name;
-            version = typstManifest.package.version;
-            src = path;
+        # Extract dependencies of a typst document from source (including the dependencies of the dependencies)
+        # extracted dependencies must be present in the registry at the moment.
+        extractDependencies =
+          {
+            this ? null,
+            path,
+          }:
+          registry:
+          flatten (
+            map (p: trace "[typst2nix] extracted dependency ${p.dep.name} from source" resolveDeps p.dep) (
+              extractDependencies' { inherit this path; } registry
+            )
+          );
 
-            buildInputs = with pkgs; [ typst-package-bunlder ];
+        # Recursively collect all dependencies of a typst package
+        resolveDeps =
+          typstPkg:
+          flatten (
+            [ typstPkg ]
+            ++ (map (
+              p: trace "[typst2nix] found ${typstPkg.name} depends on ${p.name}" (resolveDeps p)
+            ) typstPkg.passthru.typstDeps)
+          );
 
-            buildPhase = ''
-              tmpdir=$(mktemp -d)
+        toPackageName = lst: "${elemAt lst 0}_${replaceStrings [ "." ] [ "_" ] (elemAt lst 1)}";
 
-              mkdir -p "$tmpdir/packages/${namespace}/${pname}/${version}"
-              cp -r . "$tmpdir/packages/${namespace}/${pname}/${version}/"
-
-              bundler -d $tmpdir/packages -o $tmpdir/bundled.tar.gz
-            '';
-
-            installPhase = ''
-              mkdir -p $out/typst/packages/
-              tar -xvzf $tmpdir/bundled.tar.gz -C $out/typst/packages
-            '';
-
-            passthru.typstDeps = getDependencies [ "${namespace}" "${pname}" ] registery path;
-          });
-
-        # Get collected dependencies (i.e. including the dependencies of the dependencies)
-        # NOTE: An example of testing if collected dependency works: preview/anti-matter
-        # NOTE: the entire dependecy resolution is doing some sort of passive failing
-        # If a package cannot be found in registery, nothing happens.
-        # Only will typst error if it is an actual package import (i.e. not in comment section)
-        # This saves us from determining and/or wrangling with "fake import".
-        getDependencies = selfPath: registery: path:
-          flatten (map (p: [ p ] ++ (attrByPath (p.path ++ [ "passthru" "typstDeps" ]) [ ] registery))
-            (getDependencies' selfPath path));
-
-        # get dependencies of a specific package of a specific version using regex on each line.
+        # get dependencies of source on a specific package of a specific version using regex on each line.
         # regex for matching dependencies:
-        # .*@([[:alnum:]]+)/([[:alnum:]]+):([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*
-        getDependencies' = selfPath: path:
+        # .*@[[:alnum:]]+/([[:alnum:]]+):([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*
+        # returns
+        # [
+        #   "cetz"
+        #   "0.4.0"
+        # ]
+        extractDependencies' =
+          { this, path }:
+          registry:
           let
             filtered = filter (p: hasSuffix ".typ" p) (filesystem.listFilesRecursive path);
           in
-          # Order matters, otherwise `take N null` will error
-          filter (p: (p.path != null) && ((take 2 p.path) != selfPath)) (flatten (map
-            (p:
-              let
-                lines = splitString "\n" (readFile p);
-              in
-              map
-                (l: {
-                  path = match ".*@([[:alnum:]-]+)/([[:alnum:]-]+):([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*" l;
-                })
-                lines
+          filter (x: x.dep != this) (
+            flatten (
+              map (
+                # for each typst source file path
+                p:
+                let
+                  lines = splitString "\n" (readFile p);
+                in
+                map (l: {
+                  dep =
+                    let
+                      dep' = match ".*@[[:alnum:]-]+/([[:alnum:]-]+):([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*" l;
+                    in
+                    if dep' != null then registry."${(toPackageName dep')}" else null;
+                }) lines
+              ) filtered
             )
-            filtered));
-
-        listPackages = dir:
-          let
-            filtered = listToAttrs (filter ({ name, value }: value == "directory") (attrsToList (readDir dir)));
-          in
-          (mapAttrs
-            (name: type:
-              let path = dir + "/${name}";
-              in
-              # This signifies we have reached a package
-              if pathExists (path + "/typst.toml") then
-                path
-              else
-                (listPackages path)
-            )
-            filtered);
+          );
       };
-    } //
-    eachSystem defaultSystems (system:
+    }
+    // eachSystem defaultSystems (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
-        };
-        rustBin = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-analyzer" ];
-        };
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustBin;
-          rustc = rustBin;
         };
       in
       rec {
-        # nix develop
-        devShells.default = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          nativeBuildInputs = with pkgs; [ typst rustBin ];
-        };
-
         packages = {
-          typst-package-bunlder = rustPlatform.buildRustPackage {
-            pname = "typst-package-bundler";
-            version = "git";
+          # Manual is no longer available in the latest cetz
+          # cetz-manual = (
+          #   self.helpers.buildTypstDoc rec {
+          #     inherit pkgs;
+          #     src = pkgs.fetchFromGitHub {
+          #       owner = "cetz-package";
+          #       repo = "cetz";
+          #       rev = "v${version}";
+          #       hash = "sha256-XaV4g/LOFGxh8zpQGwQPZrjIdlDxuhHJZpCN4Zp7gNU=";
+          #     };
+          #     main = "./manual.typ";
+          #     version = "0.4.0";
+          #     pname = "cetz-manual";
+          #   }
+          # );
 
-            src = ./.;
-
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "typst-syntax-0.11.0" = "sha256-+9uNtoAUjFiUO9dSh2a7n+VxZUvUz7iBGGGO0F9/U/o=";
+          anti-matter-manual = (
+            self.helpers.buildTypstDoc rec {
+              inherit pkgs;
+              src = pkgs.fetchFromGitHub {
+                owner = "tingerrr";
+                repo = "anti-matter";
+                rev = "v${version}";
+                hash = "sha256-J1ByutA/0ciP4/Q1N6ZJ71YNZpOH4XjxsD0+7DHl69M=";
               };
-            };
-          };
+              main = "./docs/manual.typ";
+              version = "0.1.1";
+              pname = "anti-matter-manual";
+            }
+          );
 
-          cetz-manual = (self.helpers.buildTypstDoc rec {
-            inherit pkgs;
-            src = pkgs.fetchFromGitHub {
-              owner = "cetz-package";
-              repo = "cetz";
-              rev = "e06775f273d03b07a3ef1734defff5121c03524b";
-              hash = "sha256-zqU5GxRyAx7JtvOlH9YIyvhrbOV9o/2zWsoVe8A/Ah8=";
-            };
-            path = "./manual.typ";
-            version = "0.2.2";
-            pname = "cetz-manual";
-          });
+          physica-manual = (
+            self.helpers.buildTypstDoc rec {
+              inherit pkgs;
+              src = pkgs.fetchFromGitHub {
+                owner = "Leedehai";
+                repo = "typst-physics";
+                rev = "v${version}";
+                hash = "sha256-cMRjlmam97nl2A0SzaMUn6jDttcBE2sj90BF+jd5kpU=";
+              };
+              main = "./physica-manual.typ";
+              version = "0.9.5";
+              pname = "physica-manual";
+            }
+          );
 
-          anti-matter-manual = (self.helpers.buildTypstDoc rec {
-            inherit pkgs;
-            src = "${official-packages}/packages/preview/anti-matter/${version}";
-            path = "./docs/manual.typ";
-            version = "0.1.1";
-            pname = "anti-matter-manual";
-          });
-
-          physica-manual = (self.helpers.buildTypstDoc rec {
-            inherit pkgs;
-            src = pkgs.fetchFromGitHub {
-              owner = "Leedehai";
-              repo = "typst-physics";
-              rev = "v0.9.3";
-              hash = "sha256-XfIKa2chLeX6fmIJ8wogCktMOe1L658SkrsjNihOWbs=";
-            };
-            path = "./physica-manual.typ";
-            version = "git";
-            pname = "physica-manual";
-          });
-
-          quill-guide = (self.helpers.buildTypstDoc rec {
-            inherit pkgs;
-            src = pkgs.fetchFromGitHub {
-              owner = "Mc-Zen";
-              repo = "quill";
-              rev = "v0.2.1";
-              hash = "sha256-LkasVbT769VzUtRuFXwCJ6aSEze9Gob5rlx61Z63qf4=";
-            };
-            path = "./docs/guide/quill-guide.typ";
-            version = "git";
-            pname = "quill-guide";
-          });
+          quill-guide = (
+            self.helpers.buildTypstDoc rec {
+              inherit pkgs;
+              src = pkgs.fetchFromGitHub {
+                owner = "Mc-Zen";
+                repo = "quill";
+                rev = "v${version}";
+                hash = "sha256-/QTNzaqTUv2m5EqZI70I6nlqSo7aCR5oOWFdy3oFIZc=";
+              };
+              main = "./docs/guide/quill-guide.typ";
+              version = "0.7.1";
+              pname = "quill-guide";
+            }
+          );
         };
 
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
             hooks = {
-              nixpkgs-fmt.enable = true;
+              nixfmt-rfc-style.enable = true;
 
               shellcheck.enable = true;
               shfmt.enable = true;
 
-              rustfmt.enable = true;
-
-              # typstfmt is not working well
-              # typstfmt = mkForce {
-              #   enable = true;
-              #   name = "Typst Format";
-              #   entry = "${pkgs.typstfmt}/bin/typstfmt";
-              #   files = "\\.(typ)$";
-              # };
+              typstyle.enable = true;
             };
           };
         } // packages;
-      });
+      }
+    );
 }
